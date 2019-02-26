d3dccfebb1361ce7140157cfe5e4ee0eec62fd04
ediathome <edi@ediathome.de>
2019-02-26 18:02:05 +0100

    add translations

--- translations/de.php
+++ translations/de.php
@@ -4,6 +4,8 @@ global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{prestacollege}prestashop>prestacollege_bd1e17b646581c9c6328158e069ff16f'] = 'PrestaCollege';
 $_MODULE['<{prestacollege}prestashop>prestacollege_584f13bff7e3a3da70e25e0590e38720'] = 'Ein Modul für den Einsatz von Prestashop im Klassenzimmer';
+$_MODULE['<{prestacollege}prestashop>prestacollege_a0f39e1a61400b48c80a422bfe7bb58f'] = 'Fake-Warenkörbe';
+$_MODULE['<{prestacollege}prestashop>prestacollege_ccc3c50eeea3f46bd08e4a628cfe34c1'] = 'Erstelle die folgenden Fake-Warenkörbe';
 $_MODULE['<{prestacollege}prestashop>prestacollege_79da8ae6de67d1b6efcad3ce7b33970c'] = 'Fake-Kunden generieren';
 $_MODULE['<{prestacollege}prestashop>prestacollege_31c9e0d93b2aa1c32eeda4246889130b'] = 'Die folgenden Fake-Kunden wurden erstellt';
 $_MODULE['<{prestacollege}prestashop>db_snapshot_074302f4986d641bfc428a74e3e61028'] = 'Datenbank-Snapshot importieren';
@@ -18,6 +20,9 @@ $_MODULE['<{prestacollege}prestashop>db_snapshot_3c1c06c47a52a90ac328671c769f942
 $_MODULE['<{prestacollege}prestashop>db_snapshot_b04fbc05034b760ce60ab519cc729c9c'] = 'Diese Aktion wird einen Datenbank-Snapshot von der eingegebenen URL laden';
 $_MODULE['<{prestacollege}prestashop>db_snapshot_963f9bee88bc4dd1f9c45b42ffc3d7ec'] = 'Geben Sie eine URL von der der Snapshot geladen werden soll';
 $_MODULE['<{prestacollege}prestashop>db_snapshot_e8fe528a0417b924b507cbc1df8b04c8'] = 'Datenbank-Snapshot herunterladen';
+$_MODULE['<{prestacollege}prestashop>fake_carts_a0f39e1a61400b48c80a422bfe7bb58f'] = 'Fake-Warenkörbe';
+$_MODULE['<{prestacollege}prestashop>fake_carts_e690c09bcea69f8912e7591f7458e9a7'] = 'Anzahl der Warenkörbe';
+$_MODULE['<{prestacollege}prestashop>fake_carts_5f075ae3e1f9d0382bb8c4632991f96f'] = 'Los';
 $_MODULE['<{prestacollege}prestashop>fake_customers_79da8ae6de67d1b6efcad3ce7b33970c'] = 'Fake-Kunden generieren';
 $_MODULE['<{prestacollege}prestashop>fake_customers_7a1cc0cd216268e16060f2286198cf92'] = 'Anzahl der Kunden';
 $_MODULE['<{prestacollege}prestashop>fake_customers_5f075ae3e1f9d0382bb8c4632991f96f'] = 'Los';
