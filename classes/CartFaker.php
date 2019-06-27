56c51ddb65431e932bfd022dd383715be2b77f65
ediathome <edi@ediathome.de>
2019-06-27 16:07:39 +0200

    update

--- classes/CartFaker.php
+++ classes/CartFaker.php
@@ -4,6 +4,9 @@ class CartFaker extends AbstractFaker
 {
     public $faker;
 
+    function __construct() {
+      $this->set_customer_ids();
+    }
     public function fake_carts()
     {
         @set_time_limit(0);
@@ -43,9 +46,17 @@ class CartFaker extends AbstractFaker
 
         return $cart;
     }
+    private function set_customer_ids()
+    {
+      $q = Db::getInstance(_PS_USE_SQL_SLAVE_)->query('SELECT `id_customer` FROM `'._DB_PREFIX_.'customer`;');
+      $this->all_customer_ids = $q->getAll();
+      $this->max_customer_id = count($this->all_customer_ids) - 1;
+      return $this->all_customer_ids;
+    }
     private function customer_id()
     {
-      return 1;
+      $cind = random_int(0, $this->max_customer_id);
+      return $this->all_customer_ids[$cind];
     }
     private function quantity()
     {
@@ -60,8 +71,5 @@ class CartFaker extends AbstractFaker
         if (!is_string($loc)) {
             $loc = $this->conf['localization'];
         }
-        $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow('SELECT `id_country` FROM `'._DB_PREFIX_.'country` WHERE `iso_code` = "'.strtoupper($loc).'";');
-
-        return $result['id_country'];
     }
 }
