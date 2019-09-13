6a4b9ca9ddfaebb0f4fcbd60542ec18af5e1acf3
Martin Kolb <edi@ediathome.de>
2019-09-13 15:27:07 +0200

    refine random funcs

--- classes/AbstractFaker.php
+++ classes/AbstractFaker.php
@@ -37,4 +37,31 @@ abstract class AbstractFaker
 
         return $loc;
     }
+    protected function random_date($diff_min = '-5 years', $diff_max = 'now', $f = "Y-m-d H:i:s")
+    {
+        $fd      = $this->faker()->dateTimeBetween($diff_min, $diff_max, $timezone = null);
+        return $fd->format($f);
+    }
+    protected function g_rand()
+    {
+      $peak         = 40; // Peak at 10-o-clock
+      $stdev        = 30; // Standard deviation of two hours
+      $hoursOnClock = 90; // 24-hour clock
+
+      do // Generate gaussian variable using Box-Muller
+      {
+          $u = 2.0 * mt_rand() / mt_getrandmax()-1.0;
+          $v = 2.0*mt_rand() / mt_getrandmax() -1.0;
+          $s = $u * $u + $v * $v;
+      } while ($s > 1);
+
+      $gauss = $u * sqrt(-2.0 * log($s) / $s);
+
+      $gauss = $gauss * $stdev + $peak; // Transform to correct peak and standard deviation
+
+      while ($gauss < 0) $gauss+=$hoursOnClock; // Wrap around hours to keep the random time 
+      $result = fmod($gauss,$hoursOnClock);     // on the clock
+
+      return $result;
+    }
 }
