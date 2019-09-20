5fb34ce771175ccca41a19b18231ad9db1923e0d
Martin Kolb <edi@ediathome.de>
2019-09-20 11:57:45 +0200

    add translations

--- translations/de.php
+++ translations/de.php
@@ -9,7 +9,11 @@ $_MODULE['<{prestacollege}prestashop>prestacollege_ccc3c50eeea3f46bd08e4a628cfe3
 $_MODULE['<{prestacollege}prestashop>prestacollege_79da8ae6de67d1b6efcad3ce7b33970c'] = 'Fake-Kunden generieren';
 $_MODULE['<{prestacollege}prestashop>prestacollege_31c9e0d93b2aa1c32eeda4246889130b'] = 'Die folgenden Fake-Kunden wurden erstellt';
 $_MODULE['<{prestacollege}prestashop>prestacollege_a39cd6ab3ca64c878545a6915b6919fb'] = 'Lege einen Datenbank-Snapshot an';
+$_MODULE['<{prestacollege}prestashop>prestacollege_1f16d0333d6ebfe26a4fa8ddc6b95b7f'] = 'Snapshot erfolgreich erstellt';
+$_MODULE['<{prestacollege}prestashop>prestacollege_b87810814abe8766428f943bd93ba743'] = 'Ein Fehler ist bei der Erstellung des Snapshots aufgetreten!';
 $_MODULE['<{prestacollege}prestashop>prestacollege_c96ac2702e777ca406dfb3a37c2972a0'] = 'Importiere einen Datenbank-Snapshot';
+$_MODULE['<{prestacollege}prestashop>prestacollege_6009f7c37239c1879c63084f0006718d'] = 'Snapshot erfolgreich importiert';
+$_MODULE['<{prestacollege}prestashop>prestacollege_a1bf9f5d26c459c122678815b815e5ac'] = 'Ein Fehler ist beim Import des Snapshots aufgetreten!';
 $_MODULE['<{prestacollege}prestashop>prestacollege_7b9e9a92ef0f161ac3a601bc57b452e9'] = 'Exportiere einen Datei-Snapshot';
 $_MODULE['<{prestacollege}prestashop>prestacollege_9ba8e0d04372900b4e085d154b8027bd'] = 'Importiere einen Datei-Snapshot';
 $_MODULE['<{prestacollege}prestashop>prestacollege_e21d5f8f09650dceaab3f211564c5173'] = 'Lade einen Datenbank-Snapshot herunter';
@@ -18,6 +22,17 @@ $_MODULE['<{prestacollege}prestashop>prestacollege_9f21768597328d1aef0c9a9319de0
 $_MODULE['<{prestacollege}prestashop>prestacollege_5b11c873c726e78c4a4679eda0a03f9b'] = 'Die Datei konnte nicht gelöscht werden.';
 $_MODULE['<{prestacollege}prestashop>prestacollege_5e1d90a30b4c4fa0de011cf178d7bfbe'] = 'Snapshot erfolgreich hochgeladen';
 $_MODULE['<{prestacollege}prestashop>prestacollege_e2bd1bfc183641d485c2cc622f269c23'] = 'Es gab einen Fehler beim Upload der Datei. Bitte stellen Sie sicher, dass die max_upload_size und post_max_size Parameter in Ihrer php.ini und in Ihren Prestashop-Einstellungen für größere Uploads eingestellt sind!';
+$_MODULE['<{prestacollege}prestashop>prestacollege_7a1cc0cd216268e16060f2286198cf92'] = 'Anzahl der Kundenprofile';
+$_MODULE['<{prestacollege}prestashop>prestacollege_a314fe96c1f3bb99a5e3e106c2a303aa'] = 'Anteil weiblicher Kunden';
+$_MODULE['<{prestacollege}prestashop>prestacollege_cf685bbd8daac1878180c842b79f886d'] = 'Anteil von Besuchern';
+$_MODULE['<{prestacollege}prestashop>prestacollege_33bf5afa09876430290f96841a72ded6'] = 'Anteil an Gastbestellern';
+$_MODULE['<{prestacollege}prestashop>prestacollege_c4129501830a6ecb5ee2dfaa9a5caddf'] = 'Anteil blockierter Kunden';
+$_MODULE['<{prestacollege}prestashop>prestacollege_38f907e923b80cc8cf3c10b90fb2d339'] = 'Anteil Geschäftskunden';
+$_MODULE['<{prestacollege}prestashop>prestacollege_ffa8786dac9822d291e6f988479a6071'] = 'Anteil Newsletter-Abonnenten';
+$_MODULE['<{prestacollege}prestashop>prestacollege_14bd8523f96246be0700336deaa5f254'] = 'Anteil Newsletter-Abonnenten mit Optin';
+$_MODULE['<{prestacollege}prestashop>prestacollege_dcb3e76b89e1a6e4ce60f6c8d3e52db7'] = 'Anteil Kunden mit zweiter Adresse';
+$_MODULE['<{prestacollege}prestashop>prestacollege_f93a57df778432293ad6ad80462ad09f'] = 'Anteil Kundenprofile mit Geburtstag';
+$_MODULE['<{prestacollege}prestashop>prestacollege_2c55362ef16f8386841f4adb792805a2'] = 'Maximales Kundenalter';
 $_MODULE['<{prestacollege}prestashop>prestacollege_22b653bd8d2edcebb9e0036b4ac79262'] = 'Zurück';
 $_MODULE['<{prestacollege}prestashop>prestacollege_dbf373dd3565accf2d2fd19700f6a5ae'] = 'Ungültige Aktion';
 $_MODULE['<{prestacollege}prestashop>db_snapshot_39829b743afb70ed8dd83d4f83e50702'] = 'Datenbank-Snapshots verwalten';
@@ -29,7 +44,6 @@ $_MODULE['<{prestacollege}prestashop>fake_carts_a0f39e1a61400b48c80a422bfe7bb58f
 $_MODULE['<{prestacollege}prestashop>fake_carts_e690c09bcea69f8912e7591f7458e9a7'] = 'Anzahl der Warenkörbe';
 $_MODULE['<{prestacollege}prestashop>fake_carts_5f075ae3e1f9d0382bb8c4632991f96f'] = 'Los';
 $_MODULE['<{prestacollege}prestashop>fake_customers_79da8ae6de67d1b6efcad3ce7b33970c'] = 'Fake-Kunden generieren';
-$_MODULE['<{prestacollege}prestashop>fake_customers_7a1cc0cd216268e16060f2286198cf92'] = 'Anzahl der Kunden';
 $_MODULE['<{prestacollege}prestashop>fake_customers_5f075ae3e1f9d0382bb8c4632991f96f'] = 'Los';
 $_MODULE['<{prestacollege}prestashop>file_snapshot_5ffa5bed1782138fade664a4f4cf47df'] = 'Datei-Snapshots verwalten';
 $_MODULE['<{prestacollege}prestashop>file_snapshot_d09e203818c69dd24a0680a4c10d7487'] = 'Diese Aktion importiert einen Datei-Snapshot, der mit PrestaCollege erstellt wurde. Nutzen Sie diese Funktion VORSICHTIG, da sie Ihre DATEN IN DEN VERZEICHNISSEN ÜBERSCHREIBEN WIRD!';
@@ -44,6 +58,7 @@ $_MODULE['<{prestacollege}prestashop>modal_upload_684e0f6e0f76d950a27ce42db9bd2d
 $_MODULE['<{prestacollege}prestashop>modal_upload_9caeffedb143d4857669823404956f7c'] = 'Snapshot-Datei auswählen';
 $_MODULE['<{prestacollege}prestashop>panel_bd1e17b646581c9c6328158e069ff16f'] = 'PrestaCollege';
 $_MODULE['<{prestacollege}prestashop>panel_e0927d12896f7efe130b5e122cc7ef68'] = 'Ein Modul zum Einsatz im Klassenzimmer von Martin Kolb';
+$_MODULE['<{prestacollege}prestashop>panel_a0f39e1a61400b48c80a422bfe7bb58f'] = 'Warenkörbe faken';
 $_MODULE['<{prestacollege}prestashop>snapshottable_d4e2713d1b1725a1592f9268589f990d'] = 'Snapshot';
 $_MODULE['<{prestacollege}prestashop>snapshottable_b18c29b8470190a02813415a04a2191f'] = 'Dateigröße';
 $_MODULE['<{prestacollege}prestashop>snapshottable_21a118e185d1b27fab95f1d58ac0f5ca'] = 'Snapshot-Datei %s importieren?';
